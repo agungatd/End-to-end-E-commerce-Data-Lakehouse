99b197338d245847cbdade1a70e7bdcaf31b70ee
agungatd <agungatidhira@gmail.com>
2024-12-06 10:26:45 +0700

    dag for extract and load postgres to datalake (bronze)

--- dags/extract_postgres_to_bronze.py
+++ dags/extract_postgres_to_bronze.py
@@ -3,50 +3,97 @@ import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.ssh.operators.ssh import SSHOperator
-# from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
-import polars as pl
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_latest_cutoff(cur, table):
-    # Create a cursor
-    query = f"""
-        SELECT latest_cutoff_at FROM meta_migration
-        where table_name = '{table}'
-    """
-    cur.execute(query)
-    data = cur.fetchall()
+def get_latest_cutoff():
 
-    return data[0][0]
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        database="ecommerce",
+        user=os.getenv('PG_USER'),
+        password=os.getenv('PG_PASS'),
+        host="postgres",
+        port="5432"
+    )
+
+    # Create a cursor
+    cur = conn.cursor()
+
+    tables = ["customer_acquisition_channels",
+              "customers",
+              "inventory",
+              "order_items",
+              "orders",
+              "product_categories",
+              "products"]
+    tbl_latest_cutoff = {}
+    for table in tables:
+        query = f"""
+            SELECT latest_cutoff_at FROM meta_migration
+            where table_name = '{table}'
+        """
+        cur.execute(query)
+        data = cur.fetchone()
+
+        # set latest cutoff timestamp for each table
+        if data:
+            cutoff_at = data[0]
+        else:
+            cutoff_at = '1900-01-01 00:00:00'
+        tbl_latest_cutoff[table] = cutoff_at
+
+    # Close postgres connection
+    cur.close()
+    conn.close()
+
+    return tbl_latest_cutoff
+
 
 def set_latest_cutoff(cur, table, latest_cutoff):
     pass
 
+tables = ["customer_acquisition_channels",
+            "customers",
+            "inventory",
+            "order_items",
+            "orders",
+            "product_categories",
+            "products"]
 
 # Define the DAG
 with DAG(
     dag_id='test_extract_pg_to_bronze',
     start_date=days_ago(1),
     schedule_interval=None,  # Adjust the schedule as needed
-    catchup=False
+    catchup=False,
+    max_active_tasks=4, # Control parallelism
 ) as dag:
     
     _start = DummyOperator(task_id='dag_start')
     _end = DummyOperator(task_id='dag_end')
+    
+    with TaskGroup("extract_and_load_bronze", tooltip="Extract and Load Tables") as extract_and_load:
+        for table in tables:
+            extract_load = SSHOperator(
+                task_id=f'el_pg_to_bronze_{table}',
+                ssh_conn_id='ssh_spark',
+                command=f"""export AWS_REGION={os.getenv('AWS_REGION')} \
+                    export AWS_ACCESS_KEY_ID={os.getenv('AWS_ACCESS_KEY_ID')} \
+                    export AWS_SECRET_ACCESS_KEY={os.getenv('AWS_SECRET_ACCESS_KEY')} \
+                    export POSTGRES_TABLE={table} \
+                    && /opt/spark/bin/spark-submit /home/spark/jobs/load_pgdata_to_datalake.py     
+                """
+            )
 
-    process_customers = SSHOperator(
-        task_id='load_postgres_to_bronze',
-        ssh_conn_id='ssh_spark',
-        command=f"""export AWS_REGION={os.getenv('AWS_REGION')} \
-            && export AWS_ACCESS_KEY_ID={os.getenv('AWS_ACCESS_KEY_ID')} \
-            && export AWS_SECRET_ACCESS_KEY={os.getenv('AWS_SECRET_ACCESS_KEY')} \
-            && /opt/spark/bin/spark-submit /home/spark/jobs/load_pgdata_to_datalake.py     
-        """
-    )
+            extract_load
+
 
     # Set the task dependencies
-    _start >> process_customers >> _end
+    _start >> extract_and_load >> _end
--- spark/jobs/load_pgdata_to_datalake.py
+++ spark/jobs/load_pgdata_to_datalake.py
@@ -1,58 +1,65 @@
+import os
 from pyspark.sql import SparkSession
 
-def migrate(spark, jdbc_url, schema,  table_name):
-    # Read data from PostgreSQL
-    print("Reading data from PostgreSQL...")
-    if table_name == 'customers':
+def get_pg_df(spark, jdbc_url, query):
+    df = spark.read.format('jdbc') \
+            .option("url", jdbc_url) \
+            .option("query",query) \
+            .option("user","postgres") \
+            .option("password","postgres") \
+            .option("driver","org.postgresql.Driver") \
+            .load()
+
+    return df
+
+def get_data_latest_date(spark, jdbc_url, table, col):
+    query = f"SELECT MAX({col}) FROM {table}"
+    df = get_pg_df(spark, jdbc_url, query)
+
+    return df.first()[0]
+
+def migrate(spark, jdbc_url, schema, table):
+    if table == 'customers':
         filter_col = 'registration_date'
     else:
         filter_col = 'created_at'
+    # Set the cutoff time from latest data created datetime.
+    cutoff_time = get_data_latest_date(spark, jdbc_url, table, filter_col)
 
+    print(f"Reading data {table} with cutoff {cutoff_time} from PostgreSQL...")
     query = f"""
-        select * from {table}
-        where {filter_col} > '2020-01-05 02:23:48.000'
+        SELECT * FROM {table}
+        WHERE {filter_col} > '{cutoff_time}'
     """
-    postgres_df = spark.read.format('jdbc') \
-                    .option("url", jdbc_url) \
-                    .option("query",query) \
-                    .option("user","postgres") \
-                    .option("password","postgres") \
-                    .option("driver","org.postgresql.Driver") \
-                    .load()
+    # Read data from PostgreSQL
+    df = get_pg_df(spark, jdbc_url, query)
 
     # Show a preview of the data
-    postgres_df.show(5)
+    # df.show(5)
 
     # Write data to MinIO in Iceberg format
-    iceberg_table = f"demo.{schema}.{table_name}"
+    iceberg_table = f"demo.{schema}.{table}"
 
     print(f"Writing data to Iceberg table: {iceberg_table}")
-    postgres_df \
-        .writeTo(iceberg_table) \
-        .append() \
+    df.writeTo(iceberg_table) \
+        .append()
 
-    print("Data migration completed successfully!")
+    print(f"Data migration for table {table} completed successfully!")
 
 
 if __name__=="__main__":
+    table = os.getenv('POSTGRES_TABLE')
+
     # Initialize Spark session with necessary Iceberg and Hadoop configurations
     spark = SparkSession.builder \
-        .appName("PostgreSQL to MinIO with Iceberg DataLakehouse") \
+        .appName(f"PostgreSQL to MinIO with Iceberg DataLakehouse, table {table}") \
         .getOrCreate()
     schema = "ecommerce"
-    tables = ["customer_acquisition_channels", "customers", "inventory",
-              "order_items", "orders", "product_categories", "products"]
-    tables = ['customers']
 
     # PostgreSQL connection properties
     jdbc_url = "jdbc:postgresql://postgres/ecommerce"
-    # jdbc_properties = {
-    #     "user": "postgres",
-    #     "password": "postgres",
-    #     "driver": "org.postgresql.Driver"
-    # }
-    for table in tables:
-        migrate(spark, jdbc_url, schema, table)
+    
+    migrate(spark, jdbc_url, schema, table)
 
     # Stop spark session
     spark.stop()
