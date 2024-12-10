ff2f7a858b81927b762038d6994546fe793f0aff
agungatd <agungatidhira@gmail.com>
2024-12-10 14:54:04 +0700

    remove airflow admin creation

--- containers/airflow/entrypoint.sh
+++ containers/airflow/entrypoint.sh
@@ -3,14 +3,5 @@
 echo "Initializing the Airflow database..."
 airflow db init
 
-echo "Creating admin user..."
-airflow users create \
-    --username $AIRFLOW_ADMIN_USER \
-    --firstname $AIRFLOW_ADMIN_FIRST_NAME \
-    --lastname $AIRFLOW_ADMIN_LAST_NAME \
-    --role Admin \
-    --email $AIRFLOW_ADMIN_EMAIL \
-    --password $AIRFLOW_ADMIN_PASS
-
 echo "Starting $1..."
 exec airflow "$1"
