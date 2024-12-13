e195f338d79b49a00ccf5faacd82d899086f2963
agungatd <agungatidhira@gmail.com>
2024-12-13 16:42:56 +0700

    create template etl spark job and dag

---
+++ dags/_pipeline_template.py
@@ -0,0 +1,72 @@
+import os
+
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEFAULT_SPARK_ENV = f"""
+    export AWS_REGION={os.getenv('AWS_REGION')} \
+    export AWS_ACCESS_KEY_ID={os.getenv('AWS_ACCESS_KEY_ID')} \
+    export AWS_SECRET_ACCESS_KEY={os.getenv('AWS_SECRET_ACCESS_KEY')}
+"""
+SSH_CONN_ID = 'ssh_spark'
+
+# Define the DAG
+with DAG(
+    dag_id='_pipeline_template',
+    start_date=days_ago(1),
+    schedule=None,  # Adjust the schedule as needed
+    catchup=False,
+) as dag:
+    
+    _start = DummyOperator(task_id='dag_start')
+    _end = DummyOperator(task_id='dag_end')
+    
+    source_schema = 'DEV'
+    target_schema = 'dev_temp_raw'
+    source_table = '_temp_table'
+    target_table = '_temp_table'
+    extract_query = f'SELECT * FROM {source_table}'
+    iceberg_table = f"demo.{target_schema}.{target_table}"
+    spark_job = '_temp_extract.py'
+    extract_load_task = SSHOperator(
+        task_id=f'extract_{source_table}',
+        ssh_conn_id=SSH_CONN_ID,
+        command=f"""{DEFAULT_SPARK_ENV} \
+            export SPARK_APPNAME='{f"Template for Extracting table to table: {source_table} -> {target_table}"}' \
+            export SOURCE_TABLE={source_table} \
+            export TARGET_TABLE={target_table} \
+            export EXTRACT_QUERY='{extract_query}' \
+            export JDBC_URL={f"jdbc:postgresql://postgres/{source_schema}"} \
+            export ICEBERG_DB_TABLE={iceberg_table} \
+            && /opt/spark/bin/spark-submit /home/spark/jobs/{spark_job}     
+        """
+    )
+
+    source_schema = 'dev_temp_raw'
+    target_schema = 'dev_temp_curated'
+    source_table = '_temp_table'
+    target_table = '_temp_table'
+    extract_query = f'SELECT * FROM {source_table}'
+    iceberg_table = f"demo.{target_schema}.{target_table}"
+    spark_job = '_temp_transform.py'
+    transform_load_task = SSHOperator(
+        task_id=f'transform_{source_table}',
+        ssh_conn_id=SSH_CONN_ID,
+        command=f"""{DEFAULT_SPARK_ENV} \
+            export SPARK_APPNAME='{f"Template for Extracting table to table: {source_table} -> {target_table}"}' \
+            export SOURCE_TABLE={source_table} \
+            export TARGET_TABLE={target_table} \
+            export EXTRACT_QUERY='{extract_query}' \
+            export ICEBERG_DB_TABLE={iceberg_table} \
+            && /opt/spark/bin/spark-submit /home/spark/jobs/{spark_job}     
+        """
+    )
+
+    # Set the task dependencies
+    _start >> extract_load_task >> transform_load_task >> _end
---
+++ spark/jobs/_temp_extract.py
@@ -0,0 +1,73 @@
+import os
+from datetime import datetime
+from pyspark.sql import SparkSession
+
+
+ENV = {
+    'SPARK_APPNAME': os.getenv('SPARK_APPNAME', 'APPNAME HAS NOT BEEN SET'),
+    'EXTRACT_QUERY': os.getenv('EXTRACT_QUERY', ''),
+    'LOAD_QUERY': os.getenv('LOAD_QUERY', ''),
+    'JDBC_URL': os.getenv('JDBC_URL', ''),
+    'ICEBERG_DB_TABLE': os.getenv('ICEBERG_DB_TABLE')
+}
+
+def get_df_postgres(spark, jdbc_url, query):
+    df = spark.read.format('jdbc') \
+        .option("url", jdbc_url) \
+        .option("query",query) \
+        .option("user","postgres") \
+        .option("password","postgres") \
+        .option("driver","org.postgresql.Driver") \
+        .load()
+    return df
+
+def get_spark_df(spark, query):
+    return
+
+def get_dataframe(spark, query):
+    jdbc_url = ENV['JDBC_URL']
+    if 'postgres' in jdbc_url:
+        df = get_df_postgres(spark, jdbc_url, query)
+    else:
+        df = get_spark_df(spark, query)
+    
+    return df
+
+def transform_dataframe(df):
+    # implement spark transformations
+    return df
+
+def load_dataframe(df, iceberg_table):
+    # Write data to MinIO in Iceberg format
+    # iceberg_table = f"demo.{schema}.{table}"
+    df.writeTo(iceberg_table) \
+        .append()
+
+def etl(spark):
+    EXTRACT_QUERY = ENV['EXTRACT_QUERY']
+    ICEBERG_DB_TABLE = ENV['ICEBERG_DB_TABLE']
+
+    print(f"Extracting Data")
+    df = get_dataframe(spark, EXTRACT_QUERY)
+
+    print("Transforming Data")
+    df = transform_dataframe(df)
+
+    print(f"Writing data to Iceberg table: {ICEBERG_DB_TABLE}")
+    load_dataframe(df, ICEBERG_DB_TABLE)
+
+    print(f"Data Extraction has completed successfully!")
+
+
+if __name__=="__main__":
+    appname = ENV['SPARK_APPNAME']
+
+    # Initialize Spark session with necessary Iceberg and Hadoop configurations
+    spark = SparkSession.builder \
+        .appName(appname) \
+        .getOrCreate()
+
+    etl(spark)
+
+    # Stop spark session
+    spark.stop()
