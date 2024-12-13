77b21984f8806360137eb40ed13c5a63217e7889
agungatd <agungatidhira@gmail.com>
2024-12-13 15:49:57 +0700

    move airflow db migrate and user creation back to entrypoint.sh

--- containers/airflow/entrypoint.sh
+++ containers/airflow/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Initializing the Airflow database..."
-airflow db init
+airflow db migrate
 
+echo "Creating admin user..."
+airflow users create \
+    --username $AIRFLOW_ADMIN_USER \
+    --firstname $AIRFLOW_ADMIN_FIRST_NAME \
+    --lastname $AIRFLOW_ADMIN_LAST_NAME \
+    --role Admin \
+    --email $AIRFLOW_ADMIN_EMAIL \
+    --password $AIRFLOW_ADMIN_PASS
+    
 echo "Starting $1..."
 exec airflow "$1"
