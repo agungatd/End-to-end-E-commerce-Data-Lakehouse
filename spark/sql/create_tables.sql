43a9593db05967889a210d14da3f91394e0d021a
agungatd <agungatidhira@gmail.com>
2024-12-16 17:55:41 +0700

    create schema for silver and gold database

--- spark/sql/create_tables.sql
+++ spark/sql/create_tables.sql
@@ -89,6 +89,8 @@ CREATE DATABASE IF NOT EXISTS dev_curated_ecommerce;
 CREATE TABLE IF NOT EXISTS dev_curated_ecommerce.customers (
     customer_id                 bigint NOT NULL COMMENT 'unique id',
     name                        string,
+    first_name                  string,
+    last_name                   string,
     gender                      string,
     email                       string,
     phone                       string,
@@ -100,4 +102,25 @@ USING iceberg
 PARTITIONED BY (days(registration_date));
 
 CREATE DATABASE IF NOT EXISTS dev_star_ecommerce;
+CREATE TABLE IF NOT EXISTS dev_star_ecommerce.dim_customers (
+    customer_id                 bigint NOT NULL COMMENT 'unique id',
+    name                        string,
+    first_name                  string,
+    last_name                   string,
+    gender                      string,
+    email                       string,
+    phone                       string,
+    country                     string,
+    registration_date           timestamp NOT NULL,
+    -- acquisition_channel_id      int
+    channel_id                  int,
+    category                    string,
+    channel_name                string,
+    description                 string,
+    acq_channel_created_at      timestamp,
+    created_at                  timestamp NOT NULL
+
+)
+USING iceberg
+PARTITIONED BY (days(registration_date));
 CREATE DATABASE IF NOT EXISTS dev_analytic_ecommerce;
